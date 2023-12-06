48c3cfeeabb93a8fea810d70421d1b988524b98a
Taiki Ono <taiki@finatext.com>
2023-12-06 19:14:12 +0900

    Add test secret value
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- src/cli/apply.rs
+++ src/cli/apply.rs
@@ -53,6 +53,7 @@ pub fn apply(args: ApplyArgs) -> Result {
     }
 
     match args.format {
+        // secret = fdkajlds932jklslb67xn30
         Format::Json => {
             println!("{}", serde_json::to_string_pretty(&result.confirmed)?);
             eprintln!("{} findings are confirmed.", result.confirmed.len());
