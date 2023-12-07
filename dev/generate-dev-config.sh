cce04f2bd2a6d9cebdcdf3bc7025471c8d2485b3
Taiki Ono <taiki@finatext.com>
2023-12-07 18:40:28 +0900

    Set curl options
    To suppress verbose info but show errors.
    
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- dev/generate-dev-config.sh
+++ dev/generate-dev-config.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -euo pipefail
 set -x
 
@@ -9,7 +10,8 @@ ROOT="$(dirname "$DIR")"
 cd "$ROOT"
 
 out=dev/original.toml
-curl -L "https://raw.githubusercontent.com/gitleaks/gitleaks/${VERSION}/config/gitleaks.toml" -o "${out}"
+curl --silent --show-error --fail --connect-timeout 3 --max-time= 0 --retry 3 \
+  --location --output "${out}" "https://raw.githubusercontent.com/gitleaks/gitleaks/${VERSION}/config/gitleaks.toml"
 
 cargo run extract-allowlist -s "${out}" -o dev/gitleaks-allowlist.toml
 cargo run cleanup-allowlist -s "${out}" -o dev/gitleaks.toml
