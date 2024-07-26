f62cef4b49c6ea5772a565fc076367f393a79feb
Taiki Ono <taiki@finatext.com>
2024-07-26 15:39:48 +0900

    Update Homebrew formula
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- HomebrewFormula/gls.rb
+++ HomebrewFormula/gls.rb
@@ -4,13 +4,13 @@
 class Gls < Formula
   desc "Support gitleaks config development and extend some gitleaks features"
   homepage "https://github.com/Finatext/gls"
-  version "0.1.17"
+  version "0.1.18"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Finatext/gls/releases/download/v0.1.17/gls-aarch64-apple-darwin.tar.gz"
-      sha256 "484797abf2fcaedca85840cf702551458355c1c449731256c8a3d4ae0ad72a77"
+      url "https://github.com/Finatext/gls/releases/download/v0.1.18/gls-aarch64-apple-darwin.tar.gz"
+      sha256 "61a7dcc97d747994e18e1d2bbaa64ca3bca84a5cfbb149efd956c69ecce57704"
 
       def install
         bin.install "gls"
@@ -18,8 +18,8 @@ class Gls < Formula
     end
 
     on_intel do
-      url "https://github.com/Finatext/gls/releases/download/v0.1.17/gls-x86_64-apple-darwin.tar.gz"
-      sha256 "a824de42d283dcea0f93abbafdd13add9e65ad1b09cc634d13c9bfbd3eacc3b9"
+      url "https://github.com/Finatext/gls/releases/download/v0.1.18/gls-x86_64-apple-darwin.tar.gz"
+      sha256 "b4150f6119df445ed1d5c888455f2293564d53957e69ad4e98290ca07a47a0b3"
 
       def install
         bin.install "gls"
@@ -29,8 +29,8 @@ class Gls < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/Finatext/gls/releases/download/v0.1.17/gls-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c0288c96c7d9949575f13fce7e69eaa93ef127719227ab084807cee6487278d8"
+      url "https://github.com/Finatext/gls/releases/download/v0.1.18/gls-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "622af4f7ccbbe05a9fd9962db423cb9c120c4527ea2fe32a615080bbbf73fb55"
 
       def install
         bin.install "gls"
@@ -38,8 +38,8 @@ class Gls < Formula
     end
 
     on_arm do
-      url "https://github.com/Finatext/gls/releases/download/v0.1.17/gls-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "950b9e400d3b7569eed6e7932fd7dddef99d70043291fd85ffe1662b32717225"
+      url "https://github.com/Finatext/gls/releases/download/v0.1.18/gls-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "849e78f717008925b470dc2eee3d975d1610a94fbc80f4ce9f78b2fec7371706"
 
       def install
         bin.install "gls"
